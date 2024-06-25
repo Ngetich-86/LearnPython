eb1ef7d173605de91365cc24a28f7dd11f6d40bc
Ngetich-86 <gideonkngetich86@gmail.com>
2024-06-25 12:14:51 +0300

    Learning Python

---
+++ Numerics.py
@@ -0,0 +1,20 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue1=3.14
+
+print(myValue1)
+print(type(myValue1))
+print(str(myValue1) + " is of the data type " + str(type(myValue1)))
+
+myValue2=5j
+print(myValue2)
+print(type(myValue2))
+print(type(myValue))
